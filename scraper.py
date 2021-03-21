cde17f8f0c618e0005c1066cc5abe10a05344079
mnbvmnbv2 <32966421+mnbvmnbv2@users.noreply.github.com>
2021-03-22 00:26:25 +0100

    fixed wrong naming

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ def scrapeBook(isbn):
     r = s.get('https://www.amazon.com/dp/' + isbn)
     r.html.render(sleep=1)
 
-    books = {
+    book = {
         'title' : r.html.xpath('//*[@id="productTitle"]', first=True).text,
         'author' : r.html.xpath('//*[@id="bylineInfo"]/span/span[1]/a[1]', first=True).text,
         'publisher' : r.html.xpath('//*[@id="anonCarousel4"]/ol/li[3]/div/div[3]/span', first=True).text,
