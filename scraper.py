2cee45c91f8961ed43d76b8f90280582388b0b2d
mnbvmnbv2 <32966421+mnbvmnbv2@users.noreply.github.com>
2021-03-22 01:35:16 +0100

    made an inputlist

--- scraper.py
+++ scraper.py
@@ -1,28 +1,45 @@
 from requests_html import HTMLSession
-import numpy
+import numpy as np
 
-isbnlist = ['1599869772']
-books = [['title','author','publisher']]
+isbnlist = np.genfromtxt("isbnlist.txt", delimiter=",", dtype=str).tolist()
+
+books = [['isbn','title','author','publisher','pages','words','link']]
 
 def scrapeBook(isbn):
     s = HTMLSession()
     r = s.get('https://www.amazon.com/dp/' + isbn)
     r.html.render(sleep=1)
 
-    book = {
-        'title' : r.html.xpath('//*[@id="productTitle"]', first=True).text,
-        'author' : r.html.xpath('//*[@id="bylineInfo"]/span/span[1]/a[1]', first=True).text,
-        'publisher' : r.html.xpath('//*[@id="anonCarousel4"]/ol/li[3]/div/div[3]/span', first=True).text,
-    }
+    try:
+        book = {
+            'isbn' : isbn,
+            'title' : r.html.xpath('//*[@id="productTitle"]', first=True).text,
+            'author' : r.html.xpath('//*[@id="bylineInfo"]/span/span[1]/a[1]', first=True).text,
+            'publisher' : r.html.xpath('//*[@id="anonCarousel4"]/ol/li[3]/div/div[3]/span', first=True).text,
+            'pages' : r.html.xpath('//*[@id="anonCarousel4"]/ol/li[1]/div/div[3]/span', first=True).text,
+            'words' : str(int(r.html.xpath('//*[@id="anonCarousel4"]/ol/li[1]/div/div[3]/span', first=True).text.split(' ')[0]) * 250),
+            'link' : 'https://www.amazon.com/dp/' + isbn
+        }
+    except:
+        print('something went wrong during scraping')
+        book = {
+            'isbn' : isbn,
+            'title' : '',
+            'author' : '',
+            'publisher' : '',
+            'pages' : '',
+            'words' : '',
+            'link' : 'https://www.amazon.com/dp/' + isbn
+        }
 
     return book
 
 def outputBooklist():
     for isbn in isbnlist:
         book = scrapeBook(isbn)
-        bookline = [book['title'], book['author'], book['publisher']]
+        bookline = [book['isbn'], book['title'], book['author'], book['publisher'], book['pages'], book['words'], book['link']]
         books.append(bookline)
 
 outputBooklist()
 print(books)
-numpy.savetxt("booklist.csv", books, fmt="%s", delimiter=",")
+np.savetxt("booklist.csv", books, fmt="%s", delimiter=",")
